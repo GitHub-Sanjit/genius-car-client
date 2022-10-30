0674d5dd8153cc70420e8d1ac6018329e176fb1c
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-30 22:36:39 +0600

    66-2 Set basic Layout for Footer and header

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,9 +1,19 @@
+import Main from "../../Layout/Main";
+import Home from "../../pages/Home/Home/Home";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
-    {
-      path: '/',
-    }
-])
-  
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
 export default router;
