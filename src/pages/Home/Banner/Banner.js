5e070cdd1d93ff093952db9cf2a227244945b5ca
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-31 13:09:59 +0600

    66-6 Fix border radius and relative position image for About section

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Banner.css";
+// import "./BannerItem.css";
 import img1 from "../../../assets/images/banner/1.jpg";
 import img2 from "../../../assets/images/banner/2.jpg";
 import img3 from "../../../assets/images/banner/3.jpg";
@@ -50,24 +50,10 @@ const bannerData = [
 const Banner = () => {
   return (
     <div>
-      <div className="carousel w-full">
+      <div className="carousel w-full py-10">
         {bannerData.map((slide) => (
           <BannerItem key={slide.id} slide={slide}></BannerItem>
         ))}
-
-        {/* <BannerItem image={img1}></BannerItem> */}
-
-        {/* <div id="slide2" className="carousel-item relative w-full">
-          <img src={img2} className="w-full" alt="" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div> */}
       </div>
     </div>
   );
