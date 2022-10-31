a89e09764ab42be21efc45496198c0a184e4d14e
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-10-31 15:49:42 +0600

    66-10 (Recap) Auth Context setup and create a user and Module Summary and Home work

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import img from "../../assets/images/login/login.svg";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const SignUp = () => {
+  const { createUser } = useContext(AuthContext);
   const handleSignUp = (event) => {
     event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((err) => console.error(err));
   };
 
   return (
@@ -46,7 +58,7 @@ const SignUp = () => {
               </label>
               <input
                 name="password"
-                type="text"
+                type="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
