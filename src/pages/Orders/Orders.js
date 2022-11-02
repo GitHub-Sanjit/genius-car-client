62abccf04da39547fff3d3871814b987be64e347
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-02 17:50:37 +0600

    67-7 Display orders and Service image in a tabular format

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import OrderRow from "./OrderRow";
 
 const Orders = () => {
   const { user } = useContext(AuthContext);
-  const [orders, setOrders] = useState({});
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:5000/orders?email=${user?.email}`)
@@ -14,6 +15,29 @@ const Orders = () => {
   return (
     <div>
       <h2 className="text-5xl">You have : {orders.length} orders</h2>
+      <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <OrderRow key={order._id} order={order}></OrderRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
